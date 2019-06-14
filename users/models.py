ca2d6d3d560f754e456f5ecf726d2476f623512c
Tatu Wikman <tatu.wikman@gmail.com>
2019-06-14 22:53:03 +0300

    some fields to play with

--- users/models.py
+++ users/models.py
@@ -1,9 +1,87 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, email, first_name, last_name, phone, password):
+        user = self.model(email=email, first_name=first_name,
+                          last_name=last_name, phone=phone)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
 
 
 class CustomUser(AbstractUser):
-    name = models.CharField(blank=True, max_length=255)
+    # django kinda expects username field to exists even if we don't use it
+    username = models.CharField(
+        max_length=30,
+        unique=False,
+        blank=True,
+        null=True,
+        verbose_name="username not used",
+        help_text="django expects that we have this field..."
+    )
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        verbose_name=_('email address'),
+        help_text=_(
+            'Your email address will be used for important notifications about your membership'),
+        max_length=255,
+    )
+
+    phone = models.CharField(
+        blank=False,
+        verbose_name=_('Mobile phone number'),
+        help_text=_(
+            'This number will also be the one that gets access to the hacklab premises'),
+        max_length=255,
+    )
+
+    # some datetime bits
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="User creation date",
+        help_text="Automatically set to now when user is create"
+    )
+    last_modified = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("Last modified datetime"),
+        help_text=_("Last time this user was modified"),
+    )
+    # datetime of last payment, the payment information itself will be in its own table
+    last_payment_on = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name=_("Last payment"),
+        help_text=_(
+            "Last datetime this user had a payment transaction happen. TODO: should probably be dynamic"),
+    )
+
+    # when the member wants to leave we will mark now to this field and then have a cleanup script
+    # to remove the members information after XX days
+    marked_for_deletion_on = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name=_("Marked for deletion"),
+        help_text=_(
+            "Filled if the user has marked themself as wanting to end their membership"),
+    )
+
+    # we don't really want to get any nicknames, plain email will do better as our username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
+
+    objects = CustomUserManager()
+
+    def get_short_name(self):
+        return self.email
+
+    def natural_key(self):
+        return self.email
 
     def __str__(self):
         return self.email
