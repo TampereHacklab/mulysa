496404a44d0a4004a99c45b4b4b3b7827d04017f
Tatu Wikman <tatu.wikman@gmail.com>
2019-06-14 22:22:35 +0300

    shell_plus please

--- drfx/settings.py
+++ drfx/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
