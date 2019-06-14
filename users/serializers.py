ec71d4e87605587e402dcd6446a4dc31538f230c
Tatu Wikman <tatu.wikman@gmail.com>
2019-06-14 23:11:17 +0300

    endpoint for changing the users activation

--- users/serializers.py
+++ users/serializers.py
@@ -8,3 +8,10 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = models.CustomUser
         fields = ('url', 'email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff', 'created', 'last_modified', 'marked_for_deletion_on')
         read_only_fields = ('is_active', 'is_staff', 'created', 'last_modified', 'marked_for_deletion_on')
+
+
+class UserActivationSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.CustomUser
+        fields = ('is_active',)
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
@@ -7,3 +9,21 @@ from . import serializers
 class UserViewSet(viewsets.ModelViewSet):
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
+
+    @action(detail=True, methods=['post', 'get'])
+    def set_activation(self, request, pk=None):
+        """
+        Change the users active bit
+        """
+        user = self.get_object()
+
+        if request.data:
+            serializer = serializers.UserActivationSerializer(
+                data=request.data)
+            if not serializer.is_valid():
+                return Response(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+            user.is_active = data['is_active']
+            user.save()
+
+        return Response(serializers.UserSerializer(user, context={'request': request}).data)
