f396f57f33a49e4bb618dca4a43ec8bda88e6db8
Tatu Wikman <tatu.wikman@gmail.com>
2019-06-14 22:29:20 +0300

    admin only with debug

--- drfx/urls.py
+++ drfx/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('admin/', admin.site.urls),
+    ]
