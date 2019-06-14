d64fb24ad99c9e29e79b18165b49884eba142816
Tatu Wikman <tatu.wikman@gmail.com>
2019-06-14 22:53:27 +0300

    more fields in serializers

--- users/serializers.py
+++ users/serializers.py
@@ -4,8 +4,7 @@ from . import models
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-#    url = serializers.HyperlinkedIdentityField(view_name='user-detail', read_only=True)
-
     class Meta:
         model = models.CustomUser
-        fields = ('url', 'email', 'username', )
+        fields = ('url', 'email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff', 'created', 'last_modified', 'marked_for_deletion_on')
+        read_only_fields = ('is_active', 'is_staff', 'created', 'last_modified', 'marked_for_deletion_on')
