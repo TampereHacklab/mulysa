4836e1cadb0c8a14f4420832df1c1c4572c37ac1
Tatu Wikman <tatu.wikman@gmail.com>
2019-06-18 11:29:33 +0300

    docs

--- drfx/urls.py
+++ drfx/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('docs/', include_docs_urls(title='mylysa docs'))
 ]
 
 if settings.DEBUG:
