4d2db4c9ef57aa4f7106b7d07c7aeb8f93ab83fd
Tatu Wikman <tatu.wikman@gmail.com>
2020-01-13 14:20:40 +0200

    Fix ssl redirect fix startup (#74)
    * don't run makemigrations, fix initadmin by separating it as a command
    
    * no need to clone drfx, and our pipenv install everything already
    
    * z for selinux in volumes, add redirect middleware for http=>https
    
    * utils is not an app, commands wont be loaded from there
    
    * isort
    
    * until we blacken everything dont check for quotes

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,9 @@ RUN apt update
 RUN apt -y install gettext git python3-dev default-libmysqlclient-dev
 ENV PYTHONUNBUFFERED 1
 RUN pip install pipenv
-RUN mkdir /code 
+RUN mkdir /code
 WORKDIR /code
 ADD . /code/
-RUN git clone https://github.com/wsvincent/drfx.git ; exit 0
-WORKDIR /code/drfx
-RUN pipenv install
 WORKDIR /code
 RUN pipenv install
 ENTRYPOINT ["/code/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set +x
 pipenv run django-admin compilemessages
-pipenv run python manage.py makemigrations
 
 echo "Applying database migrations"
 pipenv run python manage.py migrate
@@ -10,7 +9,7 @@ echo "Importing memberservices data"
 pipenv run python manage.py loaddata memberservices
 
 echo "Creating default superuser if no users defined yet"
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); if(User.objects.count() == 0): User.objects.create_superuser(first_name='Admin', last_name='User', email='${ADMIN_EMAIL:-admin@email.invalid}', password='${ADMIN_PASSWORD:-password}',phone='+358000')" | pipenv run python manage.py shell
+pipenv run python manage.py initadmin
 
 echo "Starting server"
 pipenv run python -u manage.py runserver 0.0.0.0:8000 --noreload
