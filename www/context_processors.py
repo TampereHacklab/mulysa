65254493a5a57b9b2ba105845e7558d0cf99e652
Tatu Wikman <tatu.wikman@gmail.com>
2020-02-08 15:35:34 +0200

    blackened

--- www/context_processors.py
+++ www/context_processors.py
@@ -4,6 +4,5 @@ from django.conf import settings
 def external_urls(request):
     return {
         "ASSOCIATION_RULES_URL": settings.ASSOCIATION_RULES_URL,
-        "MEMBERS_GUIDE_URL": settings.MEMBERS_GUIDE_URL
+        "MEMBERS_GUIDE_URL": settings.MEMBERS_GUIDE_URL,
     }
-
