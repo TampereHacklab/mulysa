cc5a591afd1ff4eb02e16a38484d7cf237f84e36
Tatu Wikman <tatu.wikman@gmail.com>
2020-02-19 08:46:33 +0200

    Docker gunicorn nginx (#91)
    nginx for static files, gunicorn for mulysa

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ pipenv run django-admin compilemessages
 echo "Applying database migrations"
 pipenv run python manage.py migrate
 
+echo "collecting static files"
+pipenv run python manage.py collectstatic --clear --no-input
+
 echo "Importing memberservices data"
 pipenv run python manage.py loaddata memberservices
 
@@ -12,4 +15,4 @@ echo "Creating default superuser if no users defined yet"
 pipenv run python manage.py initadmin
 
 echo "Starting server"
-pipenv run python -u manage.py runserver 0.0.0.0:8000 --noreload
+pipenv run gunicorn drfx.wsgi:application --bind 0.0.0.0:8000
