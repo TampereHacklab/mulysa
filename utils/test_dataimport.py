788eb27d79a50cf3b640c565f0f77dd0378b14e6
Tatu Wikman <tatu.wikman@gmail.com>
2020-09-25 21:40:05 +0300

    New test for ss activation with tito imports

--- utils/test_dataimport.py
+++ utils/test_dataimport.py
@@ -1,13 +1,113 @@
-import unittest
 import io
+from datetime import timedelta
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+
 from .dataimport import DataImport
-from users.models import BankTransaction
+from users import models
 
 
-class TestTitoImporter(unittest.TestCase):
+class TestServiceSubscriptionContinuationWithImport(TestCase):
+    def setUp(self):
+        # one active user with subscription that is about to go overdue
+        self.user = get_user_model().objects.create_customuser(
+            first_name="FirstName",
+            last_name="LastName",
+            email="user1@example.com",
+            birthday=timezone.now(),
+            municipality="City",
+            nick="user1",
+            phone="+358123123",
+        )
+        self.memberservice = models.MemberService.objects.create(
+            name="TestService",
+            cost=10,
+            days_per_payment=30,
+        )
+        self.servicesubscription = models.ServiceSubscription.objects.create(
+            user=self.user,
+            service=self.memberservice,
+            state=models.ServiceSubscription.OVERDUE,
+            # payment is over two "days_per_payment"
+            paid_until=timezone.now().date() + timedelta(days=-50),
+            reference_number="11112",
+        )
+
+    def _createTitodata(self, date, amount):
+        datestr = date.strftime("%y%m%d")
+        amount = amount * 100
+        return {
+            "aineistotunnus": "T",
+            "tietuetunnus": "10",
+            "tietueenpituus": "188",
+            "tapahtumannumero": "1".rjust(6, "0"),
+            "arkistointitunnus": datestr.rjust(18, "0"),
+            "kirjauspaiva": datestr,
+            "arvopaiva": datestr,
+            "maksupaiva": datestr,
+            "tapahtumatunnus": "1",  # 1 pano, 2 otto, 3 panon korjaus, 4 oton korjaus
+            "kirjausselitekoodi": "719",
+            "kirjausselite": "Viitemaksu".ljust(35),
+            "rahamaaraetumerkki": "+",
+            "rahamaara": str(amount).rjust(18, "0"),  # two decimals
+            "kuittikoodi": " ",  # assumed to be empty
+            "valitystapa": " ",  # assumed to be empty
+            "saaja_maksaja": "TESTI HENKIL[".ljust(35),
+            "nimen_lahde": "J",  # no idea what this means
+            "saajan_tili": "".ljust(14),  # seems to be empty when money comes in
+            "saajan_tili_muuttunut": " ",  # yup, no idea
+            "viite": "11112".rjust(20, "0"),
+            "lomakkeen_numero": "".ljust(8),  # yup, no idea again
+            "tasotunnus": " ",  # empty means that it is really in the account :)
+        }
+
+    def test_user_gets_more_time(self):
+        print(self.servicesubscription.paid_until)
+
+        paid_delta = -50
+        # starts of as overdue
+        self.assertEqual(self.servicesubscription.state, models.ServiceSubscription.OVERDUE)
+        self.assertEqual(self.servicesubscription.paid_until, timezone.now().date() + timedelta(days=paid_delta))
+
+        # makes one payment two days ago, still overdue but the date has changed
+        data = self._createTitodata(timezone.now().date() + timedelta(days=-2), 10)
+        dataline = "".join(data.values())
+        lines = io.BytesIO(b"header\n" + dataline.encode())
+        results = DataImport.import_tito(lines)
+        self.assertDictEqual(
+            results, {"imported": 1, "exists": 0, "error": 0, "failedrows": []}
+        )
+        paid_delta += 30
+
+        self.servicesubscription.refresh_from_db()
+        self.assertEqual(self.servicesubscription.state, models.ServiceSubscription.OVERDUE)
+        self.assertEqual(self.servicesubscription.paid_until, timezone.now().date() + timedelta(days=paid_delta))
+
+        # makes another payment yesterday, state PAID and date in future
+        data = self._createTitodata(timezone.now().date() + timedelta(days=-1), 10)
+        dataline = "".join(data.values())
+        lines = io.BytesIO(b"header\n" + dataline.encode())
+        results = DataImport.import_tito(lines)
+        self.assertDictEqual(
+            results, {"imported": 1, "exists": 0, "error": 0, "failedrows": []}
+        )
+        paid_delta += 30
+
+        self.servicesubscription.refresh_from_db()
+        self.assertEqual(self.servicesubscription.state, models.ServiceSubscription.ACTIVE)
+        self.assertEqual(self.servicesubscription.paid_until, timezone.now().date() + timedelta(days=paid_delta))
+
+    def tearDown(self):
+        self.user.delete()
+        self.memberservice.delete()
+        self.servicesubscription.delete()
+        models.BankTransaction.objects.all().delete()
+
+class TestTitoImporter(TestCase):
     def _getbasetitodata(self):
         """
         TODO: this should really be its own class that knows how to
@@ -43,7 +143,7 @@ class TestTitoImporter(unittest.TestCase):
         """
         Test that the generated data above works
         """
-        BankTransaction.objects.all().delete()
+        models.BankTransaction.objects.all().delete()
         data = self._getbasetitodata()
         dataline = "".join(data.values())
         lines = io.BytesIO(b"header\n" + dataline.encode())
@@ -51,10 +151,10 @@ class TestTitoImporter(unittest.TestCase):
         self.assertDictEqual(
             results, {"imported": 1, "exists": 0, "error": 0, "failedrows": []}
         )
-        self.assertEqual(BankTransaction.objects.first().reference_number, data['viite'].strip().strip("0"))
+        self.assertEqual(models.BankTransaction.objects.first().reference_number, data['viite'].strip().strip("0"))
 
     def test_tito_import_wrong_length(self):
-        BankTransaction.objects.all().delete()
+        models.BankTransaction.objects.all().delete()
         data = self._getbasetitodata()
         # remove one character from the end
         dataline = "".join(data.values())[:-1]
@@ -94,7 +194,7 @@ class TestTitoImporter(unittest.TestCase):
         this data is almost direct from real world data, some values have just
         been changed so that the persons identiy is not compromised
         """
-        BankTransaction.objects.all().delete()
+        models.BankTransaction.objects.all().delete()
         data = io.BytesIO(
             b"header\nT101880000011111111111111111112009252009252009251"
             b"710Viitemaksu                         +000000000000012300  "
@@ -119,7 +219,7 @@ class TestTitoImporter(unittest.TestCase):
         )
 
 
-class TestHolviImporter(unittest.TestCase):
+class TestHolviImporter(TestCase):
     def test_holvi_import(self):
         xls = open("utils/holvi-account-test-statement.xls", "rb")
         name = xls.name
@@ -136,4 +236,4 @@ class TestHolviImporter(unittest.TestCase):
         )
 
     def tearDown(self):
-        BankTransaction.objects.all().delete()
+        models.BankTransaction.objects.all().delete()
