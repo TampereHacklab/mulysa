cc54d76f7166ca020576983686c56b496450e455
Tatu Wikman <tatu.wikman@gmail.com>
2020-10-12 23:19:39 +0300

    Only load memberservices if none exist (#257)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ pipenv run python manage.py migrate
 echo "collecting static files"
 pipenv run python manage.py collectstatic --clear --no-input
 
-echo "Importing memberservices data"
-pipenv run python manage.py loaddata memberservices
+echo "Importing initial memberservices data if none exists"
+pipenv run python manage.py initmemberservices
 
 echo "Creating default superuser if no users defined yet"
 pipenv run python manage.py initadmin
---
+++ users/management/commands/initmemberservices.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+from users.models import MemberService
+
+
+class Command(BaseCommand):
+    help = "Create initial memberservices if no memberservices exist"
+
+    def handle(self, *args, **options):
+
+        if MemberService.objects.count() == 0:
+            print("Initializing initial memberservices")
+            call_command('loaddata', 'memberservices')
+        else:
+            print("Memberservices already initialized")
