bb9b8d19cd5cdf21d4ddba4ce0d6f6bb799aae19
Tatu Wikman <tatu.wikman@gmail.com>
2021-10-30 15:25:28 +0300

    move application management to django admin views (#343)

--- drfx/urls.py
+++ drfx/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from oauth2_provider.urls import app_name, base_urlpatterns, oidc_urlpatterns
 
 from rest_framework.documentation import include_docs_urls
 
@@ -12,5 +13,5 @@ urlpatterns = [
     path("docs/", include_docs_urls(title="mulysa docs")),
     path("www/", include("www.urls")),
     path("email/", include("emails.urls")),
-    path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path("o/", include((base_urlpatterns + oidc_urlpatterns, app_name), namespace="oauth2_provider")),
 ]
