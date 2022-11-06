c662cf73128f77d447432b573a5edda8138fa1b5
Tatu Wikman <tatu.wikman@gmail.com>
2022-11-06 15:02:18 +0200

    add fetching .map files locally to update_local_bootsrap. Fixes #402 (#403)
    * add fetching .map files locally to update_local_bootsrap
    
    fetch the map files and update bootstrap
    
    * remove empty file
    
    * fix flake8 and tox

--- www/management/commands/update_local_bootstrap.py
+++ www/management/commands/update_local_bootstrap.py
@@ -36,12 +36,15 @@ class Command(BaseCommand):
                 filename = self._fetch_and_save(url)
                 # build the settings section for it
                 newsettings[key] = f"{self.localurlbase}{filename}"
+                # check if there is a map file and get that too
+                self._fetch_and_save(f"{url}.map")
 
             href = val.get("href")
             if href:
                 filename = self._fetch_and_save(href)
                 # build the settings section for it
                 newsettings[key] = f"{self.localurlbase}{filename}"
+                self._fetch_and_save(f"{href}.map")
 
         print("Done, now you can update your settings_local to use these files:")
         print("BOOTSTRAP4 = ")
