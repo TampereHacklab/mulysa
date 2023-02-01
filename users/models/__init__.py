63a892b3791291232b1b013f85d55ef311bb86ed
Brage Sekse Aarset <brage@bjerk.io>
2023-02-01 08:19:51 +0200

    fix: set __all__ in init.py

--- users/models/__init__.py
+++ users/models/__init__.py
@@ -7,3 +7,6 @@ from users.models.service_subscription import ServiceSubscription
 from users.models.member_service import MemberService
 from users.models.users_log import UsersLog
 from users.validators import validate_agreement, validate_mxid, validate_phone
+
+__all__ = [BankTransaction, CustomInvoice, CustomUser, MembershipApplication, NFCCard,
+           ServiceSubscription, MemberService, UsersLog, validate_agreement, validate_mxid, validate_phone]
