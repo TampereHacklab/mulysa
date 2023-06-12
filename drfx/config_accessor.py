76f7b773edcfd15c2e8516b8ecc8a20b3ec93455
Brage Sekse Aarset <brage@bjerk.io>
2023-06-12 11:39:20 +0300

    fix: format

--- drfx/config_accessor.py
+++ drfx/config_accessor.py
@@ -1,6 +1,7 @@
 from constance import config
 from django.conf import settings
 
+
 # A class that accesses configuration variables from constance or settings
 class ConfigAccessor(object):
     def __getattr__(self, key):
