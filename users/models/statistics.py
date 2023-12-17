142ccc17595c2f8c072728c5e8202cdec86664a2
Tatu Wikman <tatu.wikman@gmail.com>
2023-12-17 17:39:07 +0200

    servicesubscription data collection

--- users/models/statistics.py
+++ users/models/statistics.py
@@ -5,9 +5,9 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
 from datetime import datetime
-from . import MembershipApplication, CustomInvoice
+from . import MembershipApplication, CustomInvoice, MemberService
 
-from django.db.models import Sum
+from django.db.models import Sum, Count, Q
 
 logger = logging.getLogger(__name__)
 
@@ -25,15 +25,42 @@ class StatisticsManager(models.Manager):
 
         If today already has a daily stat object will update the values for today.
         """
+        subscriptions = (
+            MemberService.objects.all()
+            .values("pk", "name")
+            .annotate(
+                total=Count("pk"),
+                active=Count(
+                    "servicesubscription", filter=Q(servicesubscription__state="ACTIVE")
+                ),
+                overdue=Count(
+                    "servicesubscription",
+                    filter=Q(servicesubscription__state="OVERDUE"),
+                ),
+                suspended=Count(
+                    "servicesubscription",
+                    filter=Q(servicesubscription__state="SUSPENDED"),
+                ),
+            )
+        )
+
+        # collect service data for each different state
         self.update_or_create(
             date=datetime.now().date(),
             defaults={
                 "users_total": get_user_model().objects.all().count(),
                 "users_active": get_user_model().objects.filter(is_active=1).count(),
+                "users_marked_for_deletion": get_user_model()
+                .objects.filter(marked_for_deletion_on__isnull=False)
+                .count(),
                 "open_membership_applications": MembershipApplication.objects.all().count(),
-
-                "custom_invoices_open": CustomInvoice.objects.filter(payment_transaction=None).count(),
-                "custom_invoices_open_sum": CustomInvoice.objects.filter(payment_transaction=None).aggregate(total=Sum('amount'))['total'],
+                "custom_invoices_open": CustomInvoice.objects.filter(
+                    payment_transaction=None
+                ).count(),
+                "custom_invoices_open_sum": CustomInvoice.objects.filter(
+                    payment_transaction=None
+                ).aggregate(total=Sum("amount"))["total"],
+                "service_subscriptions": list(subscriptions),
             },
         )
 
@@ -51,13 +78,27 @@ class Statistics(models.Model):
         primary_key=True,
         verbose_name="Date of this statistics event",
     )
-    users_total = models.IntegerField(verbose_name="Total number of users")
-    users_active = models.IntegerField(verbose_name="Active users")
+    users_total = models.IntegerField(default=0, verbose_name="Total number of users")
+    users_active = models.IntegerField(default=0, verbose_name="Active users")
+    users_marked_for_deletion = models.IntegerField(
+        default=0, verbose_name="Users pending deletion"
+    )
 
-    open_membership_applications = models.IntegerField(verbose_name="Number of open membership applications")
+    open_membership_applications = models.IntegerField(
+        default=0, verbose_name="Number of open membership applications"
+    )
 
-    custom_invoices_open = models.IntegerField(verbose_name="Number of open custom invoices")
-    custom_invoices_open_sum = models.FloatField(verbose_name="Total amount of open custom invoices")
+    custom_invoices_open = models.IntegerField(
+        default=0, verbose_name="Number of open custom invoices"
+    )
+    custom_invoices_open_sum = models.FloatField(
+        default=0, verbose_name="Total amount of open custom invoices"
+    )
+
+    service_subscriptions = models.JSONField(
+        default=list,
+        verbose_name="Statistics for service subscriptions. Each service will be its own key with counts for Active, Overdue and Suspended states",
+    )
 
     def __str__(self):
         return f"Statistics for {self.date}"
