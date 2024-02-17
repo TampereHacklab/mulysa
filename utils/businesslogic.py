ce85e54fc1a36db4b63f53fbe6d86420ab54ccf0
Erkki Hietaranta <erkki.hietaranta@gmail.com>
2024-02-17 18:27:21 +0200

    Buisness logic debug log comments updated
    Signed-off-by: Erkki Hietaranta <erkki.hietaranta@gmail.com>

--- utils/businesslogic.py
+++ utils/businesslogic.py
@@ -376,6 +376,10 @@ class BusinessLogic:
                     f"Transaction is new and enough for service {subscription.service}"
                 )
 
+                logger.debug(
+                    f"Transaction is new and enough for service {subscription.service}"
+                )
+
                 inlimit = BusinessLogic._service_maxdays_after_payment_inlimit(
                     subscription,
                     transaction,
@@ -401,7 +405,7 @@ class BusinessLogic:
                     f"Amount insufficient to pay service {subscription.service}\n"
                 )
                 transaction.save()
-                logger.debug(f"Transaction does not pay service {subscription.service}")
+
 
     @staticmethod
     def _transaction_pays_service(transaction, service):
