80bc71e3a8eb05bae173b9ca77309e7009e7adf0
Ville Ranki <ville.ranki@iki.fi>
2025-02-01 18:23:17 +0200

    Black-formatointi

--- utils/matrixoperations.py
+++ utils/matrixoperations.py
@@ -3,7 +3,9 @@ from nio import AsyncClient
 import requests
 import logging
 import asyncio
+
 logger = logging.getLogger(__name__)
+
 
 class MatrixOperations:
     """
@@ -15,12 +17,17 @@ class MatrixOperations:
     def __init__(self, access_token, matrix_server):
         self.access_token = access_token
         self.matrix_server = matrix_server
-        self.headers = {'Authorization': f'Bearer {self.access_token}', 'Content-Type': 'application/json'}
+        self.headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+        }
         self.client = AsyncClient(matrix_server)
         self.client.access_token = access_token
 
     def send_message(self, room_id, message):
-        api_url = f'{self.matrix_server}_matrix/client/v3/rooms/{room_id}/send/m.room.message'
+        api_url = (
+            f"{self.matrix_server}_matrix/client/v3/rooms/{room_id}/send/m.room.message"
+        )
         payload = {"msgtype": "m.text", "body": message}
         r = requests.post(api_url, headers=self.headers, json=payload)
         if r.status_code == 200:
@@ -37,10 +44,12 @@ class MatrixOperations:
         loop.close()
 
     def kick_user(self, user_id, room_id, reason):
-        api_url = f'{self.matrix_server}_matrix/client/v3/rooms/{room_id}/kick'
+        api_url = f"{self.matrix_server}_matrix/client/v3/rooms/{room_id}/kick"
         payload = {"reason": reason, "user_id": user_id}
         r = requests.post(api_url, headers=self.headers, json=payload)
         if r.status_code == 200:
-            logger.debug(f"Kicked Matrix user {user_id} from room {room_id} because of {reason}")
+            logger.debug(
+                f"Kicked Matrix user {user_id} from room {room_id} because of {reason}"
+            )
         else:
             logger.warn(f"Kicking Matrix user {user_id} failed:", r.content)
