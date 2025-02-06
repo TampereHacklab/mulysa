fee51619cad571e5a92490f9c1cf27da7c2d2fcc
Ville Ranki <ville.ranki@iki.fi>
2025-02-06 16:07:10 +0200

    Access method to door api log, show only NFC in NFC ui (#555)
    * Method to access log entry
    
    * Lisätty nfc testiin

--- www/views.py
+++ www/views.py
@@ -273,6 +273,7 @@ def usersettings(request, id):
             granted=False,
             nfccard=None,
             claimed_by=None,
+            method='nfc',
             date__gte=timezone.now() - timedelta(minutes=5),
         )
         .exclude(payload__isnull=True)
