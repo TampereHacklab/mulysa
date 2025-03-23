79e0d60011641ff8d2f98f31b56374d76ff3527f
Tatu Wikman <tatu.wikman@gmail.com>
2025-03-23 11:29:27 +0200

    add filter to see duplicate archival_reference transations

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils import timezone
+from django.db.models import Count
 
 from rangefilter.filters import DateRangeFilter
 
@@ -187,6 +188,27 @@ class AmountDirectionFilter(admin.SimpleListFilter):
             return queryset.filter(amount=0)
         return queryset
 
+class DuplicateTransactionIdFilter(admin.SimpleListFilter):
+    title = "Duplicate Archival reference"
+    parameter_name = "duplicate_archival_reference"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("yes", "Has duplicates"),
+        ]
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "yes":
+            duplicates = (
+                queryset
+                .values("archival_reference")
+                .annotate(txn_count=Count("id"))
+                .filter(txn_count__gt=1)
+                .values_list("archival_reference", flat=True)
+            )
+            return queryset.filter(archival_reference__in=list(duplicates))
+        return queryset
 
 class BankTransactionAdmin(admin.ModelAdmin):
     list_display = [
@@ -199,7 +221,7 @@ class BankTransactionAdmin(admin.ModelAdmin):
         "sender",
         "has_been_used",
     ]
-    list_filter = (AmountDirectionFilter, "has_been_used", "date")
+    list_filter = (AmountDirectionFilter, "has_been_used", "date", DuplicateTransactionIdFilter)
     ordering = ("-date",)
     search_fields = (
         "user__email",
