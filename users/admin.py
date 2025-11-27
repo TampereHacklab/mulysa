765b4e6b0ae9e3dd1ac4fc4bdd97cfdb077d1b15
Mikko <mikkopaloviita92@gmail.com>
2025-11-27 16:26:50 +0200

    id removed from users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -40,7 +40,7 @@ class CustomUserAdmin(UserAdmin):
         "first_name",
         "last_name",
     )
-    list_display = ( "id",
+    list_display = (
         "email",
         "first_name",
         "last_name",
